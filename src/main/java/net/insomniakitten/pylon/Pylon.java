b404727c5b9a570adaba0d4d36a4b9e908ea47d3
InsomniaKitten <admin@designdevils.co.uk>
2018-09-30 14:26:11 +0100

    Remove constructor exception

--- src/main/java/net/insomniakitten/pylon/Pylon.java
+++ src/main/java/net/insomniakitten/pylon/Pylon.java
@@ -3,7 +3,5 @@ package net.insomniakitten.pylon;
 public final class Pylon {
     public static final String VERSION = "0.3.0";
 
-    private Pylon() {
-        throw new UnsupportedOperationException("Cannot instantiate " + this.getClass());
-    }
+    private Pylon() {}
 }
