a4001c808d2a68fb81c67cca1910ef1fe5f132e1
InsomniaKitten <admin@designdevils.co.uk>
2018-11-15 13:58:19 +0000

    Fix version

--- src/main/java/net/insomniakitten/pylon/Pylon.java
+++ src/main/java/net/insomniakitten/pylon/Pylon.java
@@ -1,7 +1,7 @@
 package net.insomniakitten.pylon;
 
 public final class Pylon {
-    public static final String VERSION = "0.3.1";
+    public static final String VERSION = "0.3.2";
 
     private Pylon() {}
 }
